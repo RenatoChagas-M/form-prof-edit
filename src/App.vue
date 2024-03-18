2158fa285825131fc4050815eddbcdcf45b2e96c
Renato Chagas <renatochagas.m@gmail.com>
2024-03-18 13:42:27 -0300

    feat: option v-for

--- src/App.vue
+++ src/App.vue
@@ -22,33 +22,33 @@ function saveprofile() {
 }
 
 const allStates = ref([
-  'AC',
-  'AL',
-  'AP',
-  'AM',
-  'BA',
-  'CE',
-  'DF',
-  'ES',
-  'GO',
-  'MA',
-  'MT',
-  'MS',
-  'MG',
-  'PA',
-  'PB',
-  'PR',
-  'PE',
-  'PI',
-  'RJ',
-  'RN',
-  'RS',
-  'RO',
-  'RR',
-  'SC',
-  'SP',
-  'SE',
-  'TO',
+  {'nome':'Acre', 'sigla': 'AC'},
+  {'nome':'Alagoas', 'sigla': 'AL'},
+  {'nome':'Amapá', 'sigla': 'AP'},
+  {'nome':'Amazonas', 'sigla': 'AM'},
+  {'nome':'Bahia', 'sigla': 'BA'},
+  {'nome':'Ceará', 'sigla': 'CE'},
+  {'nome':'Distrito Federal', 'sigla': 'DF'},
+  {'nome':'Espírito Santo', 'sigla': 'ES'},
+  {'nome':'Goiás', 'sigla': 'GO'},
+  {'nome':'Maranhão', 'sigla': 'MA'},
+  {'nome':'Mato Grosso', 'sigla': 'MT'},
+  {'nome':'Mato Grosso do Sul', 'sigla': 'MS'},
+  {'nome':'Minas Gerais', 'sigla': 'MG'},
+  {'nome':'Pará', 'sigla': 'PA'},
+  {'nome':'Paraíba', 'sigla': 'PB'},
+  {'nome':'Paraná', 'sigla': 'PR'},
+  {'nome':'Pernambuco', 'sigla': 'PE'},
+  {'nome':'Piauí', 'sigla': 'PI'},
+  {'nome':'Rio de Janeiro', 'sigla': 'RJ'},
+  {'nome':'Rio Grande do Norte', 'sigla': 'RN'},
+  {'nome':'Rio Grande do Sul', 'sigla': 'RS'},
+  {'nome':'Rondônia', 'sigla': 'RO'},
+  {'nome':'Roraima', 'sigla': 'RR'},
+  {'nome':'Santa Catarina', 'sigla': 'SC'},
+  {'nome':'São Paulo', 'sigla': 'SP'},
+  {'nome':'Sergipe', 'sigla': 'SE'},
+  {'nome':'Tocantins', 'sigla': 'TO'}
 ])
 
 </script>
@@ -100,7 +100,7 @@ const allStates = ref([
           <div class="user-state">
             <label for="whichState">which stste do you live?</label>
             <select name="States">
-              <option v-for=""></option>
+              <option v-for="(value, index) of allStates" :key="index">{{value.sigla}}-{{value.nome}}</option>
             </select>
           </div>
 
