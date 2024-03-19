0e2c80198f6e303e93300dec50c4110dd7fc59d9
RenatoChagas <renatochagas.m@mgmail.com>
2024-03-19 13:49:16 -0300

    feat: script e style change

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,6 @@ const confirmPassword = ref("")
 
 const userCity = ref([""])
 
-const showProfile = ref(false)
-
-function saveprofile() {
-  showProfile.value = true
-}
-
 const allStates = ref([
   { 'name': 'Estado', 'sigla': '' },
   { 'name': 'Acre', 'sigla': 'AC-' },
@@ -67,13 +61,21 @@ function saveProfileImg(e) {
   }
 }
 
+const showFormP1 = ref(true)
+const showFormP2 = ref(false)
+
+const alterarBooleano = () => {
+  showFormP1.value = !showFormP1.value
+  showFormP2.value = !showFormP2.value
+}
+
 </script>
 
 <template>
   <main>
     <aside>
       <transition name="form" mode="out-in">
-        <section v-if="showProfile" class="user-form">
+        <section v-if="!showFormP1 && !showFormP2" class="user-form">
           <div class="userSaved-informations">
 
             <div class="saved-information-title">
@@ -87,111 +89,120 @@ function saveProfileImg(e) {
           </div>
 
           <div class="back-to-form">
-            <button @click="showProfile = false">Back</button>
+            <button @click="showFormP2 = !showFormP2">Back</button>
           </div>
         </section>
 
+        <div v-else @submit.prevent="" validate class="two-forms">
+          <transition>
+            <form v-if="showFormP2">
 
-        <form v-else @submit.prevent="saveprofile()" validate>
-          <div class="user-information">
-            <div class="user-name">
-              <input v-model="infoUser.name" placeholder="Please state your name" type="text" required>
-            </div>
+              <div class="check-boxes">
+                <div class="user-hobbies">
+                  <label for="hobbies">State your hobbies:</label>
+                  <input type="checkbox" value="read">Ler
+                  <input v-model="infoUser.hobbies" type="checkbox" value="study">Estudar
+                  <input v-model="infoUser.hobbies" type="checkbox" value="play">Jogar
+                  <input v-model="infoUser.hobbies" type="checkbox" value="watch">Assistir
+                  <input v-model="infoUser.hobbies" type="checkbox" value="sporty">Esportes
+                </div>
 
-            <div class="user-email">
-              <input v-model="infoUser.email" placeholder="Please state your email" type="email" required>
-            </div>
+                <div class="user-prog-lang">
+                  <label for="hobbies">State your favorite language:</label>
+                  <input v-model="infoUser.progLang" type="radio" value="js">JavaScript
+                  <input v-model="infoUser.progLang" type="radio" value="py">Python
+                  <input v-model="infoUser.progLang" type="radio" value="java">Java
+                  <input v-model="infoUser.progLang" type="radio" value="c">C
+                </div>
 
-            <div class="user-password">
-              <input v-model="infoUser.password" placeholder="Please state your password" type="password" required>
-            </div>
-
-            <div class="user-verify-password">
-              <input v-model="confirmPassword" placeholder="Please confirm your password" type="password" required>
-              <p v-if="infoUser.password !== confirmPassword">try again, passwords dont match</p>
-            </div>
-
-            <div class="user-born">
-              <input v-model="infoUser.born" placeholder="Please state your born date" type="date" required>
-            </div>
-
-            <div class="user-address">
-              <input v-model="infoUser.address" placeholder="Please state your address" type="text" required>
-            </div>
-
-            <div class="user-city">
-              <input v-model="userCity" placeholder="Please state your city" type="text" required>
-            </div>
-
-            <div class="user-state">
-              <label for="whichState">which stste do you live?</label>
-              <select name="States">
-                <option v-for="(value, index) of allStates" :key="index">{{ value.sigla }}{{ value.name }}</option>
-              </select>
-            </div>
-
-            <div class="check-boxes">
-              <div class="user-hobbies">
-                <label for="hobbies">State your hobbies:</label>
-                <input type="checkbox" value="read">Ler
-                <input v-model="infoUser.hobbies" type="checkbox" value="study">Estudar
-                <input v-model="infoUser.hobbies" type="checkbox" value="play">Jogar
-                <input v-model="infoUser.hobbies" type="checkbox" value="watch">Assistir
-                <input v-model="infoUser.hobbies" type="checkbox" value="sporty">Esportes
               </div>
 
-              <div class="user-prog-lang">
-                <label for="hobbies">State your favorite language:</label>
-                <input v-model="infoUser.progLang" type="radio" value="js">JavaScript
-                <input v-model="infoUser.progLang" type="radio" value="py">Python
-                <input v-model="infoUser.progLang" type="radio" value="java">Java
-                <input v-model="infoUser.progLang" type="radio" value="c">C
+              <div class="profile-image">
+                <input type="file" id="profileImg" accept="image/*" @change="saveProfileImg(e)">
               </div>
-            </div>
 
-            <div class="profile-image">
-              <input type="file" id="profileImg" accept="image/*" @change="saveProfileImg(e)">
-            </div>
+              <div class="biography">
+                <textarea name="bio" cols="50" rows="10" required></textarea>
+              </div>
 
-            <div class="biography">
-              <textarea name="bio" cols="50" rows="10" required></textarea>
-            </div>
-          </div>
+              <div class="botao">
+                <button @click="alterarBooleano()">Voltar</button>
+              </div>
+              <div class="botao">
+                <button @click="showFormP2 = !showFormP2">Enviar</button>
+              </div>
 
-          <div class="sendButon">
-            <button class="save-send-button" type="submit"
-              :disabled="infoUser.password !== confirmPassword ? true : false">Send</button>
-          </div>
-        </form>
 
+            </form>
+
+            <form v-else-if="showFormP1">
+              <div class="user-information">
+                <div class="user-name">
+                  <input v-model="infoUser.name" placeholder="Please state your name" type="text" required>
+                </div>
+
+                <div class="user-email">
+                  <input v-model="infoUser.email" placeholder="Please state your email" type="email" required>
+                </div>
+
+                <div class="user-password">
+                  <input v-model="infoUser.password" placeholder="Please state your password" type="password" required>
+                </div>
+
+                <div class="user-verify-password">
+                  <input v-model="confirmPassword" placeholder="Please confirm your password" type="password" required>
+                  <p v-if="infoUser.password !== confirmPassword">try again, passwords dont match</p>
+                </div>
+
+                <div class="user-born">
+                  <input v-model="infoUser.born" placeholder="Please state your born date" type="date" required>
+                </div>
+
+                <div class="user-address">
+                  <input v-model="infoUser.address" placeholder="Please state your address" type="text" required>
+                </div>
+
+                <div class="user-city">
+                  <input v-model="userCity" placeholder="Please state your city" type="text" required>
+                </div>
+              </div>
+
+              <div class="sendButon">
+                <button class="save-send-button" @click="alterarBooleano()">Next</button>
+              </div>
+            </form>
+          </transition>
+        </div>
       </transition>
     </aside>
   </main>
 </template>
 
 <style scoped>
-
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-*{
-  color: white;
-}
-
 main{
+  align-items: center;
   display: flex;
   justify-content: end;
-  margin: 0vw 0vw 0px 0vw;
   width: 100vw;
   height: 100vh;
+  background-image: url(assets/images/fundo-de-pintura-abstrata-com-pintura-em-aquarela-multicolorida-gerada-por-ia_188544-15558.avif);
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
 aside{
-
-  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  margin: 0px 100px 0px 0px;
+  width: 30vw;
+  height: 40vh;
 }
-
 </style>
